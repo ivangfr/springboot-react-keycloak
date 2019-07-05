abd7f2c71fd449a1642eb32f9bdafea4f4981e60
Ivan Franchin <ivan.franchin@takeaway.com>
2019-07-05 14:08:36 +0200

    remove unsued imports

--- movies-api/src/main/java/com/mycompany/moviesapi/config/CorsConfig.java
+++ movies-api/src/main/java/com/mycompany/moviesapi/config/CorsConfig.java
@@ -1,14 +1,12 @@
 package com.mycompany.moviesapi.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
