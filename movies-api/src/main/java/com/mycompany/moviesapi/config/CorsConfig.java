e317319aee6754e0dee035516da96008333e99ac
Ivan Franchin <ivan.franchin@takeaway.com>
2021-01-19 20:15:57 -0300

    project update - update to keycloak 12.0.2; - update to spring-boot 2.4.2; - update to springdoc openapi 1.5.2; - update movies-ui npm dependencies; - set CORS AllowedOrigins as property in application.yml.

--- movies-api/src/main/java/com/mycompany/moviesapi/config/CorsConfig.java
+++ movies-api/src/main/java/com/mycompany/moviesapi/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.mycompany.moviesapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -7,16 +8,17 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    CorsFilter corsFilter() {
+    CorsFilter corsFilter(@Value("${app.cors.allowed-origins}") List<String> allowedOrigins) {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
--- movies-api/src/main/java/com/mycompany/moviesapi/repository/MovieRepository.java
+++ movies-api/src/main/java/com/mycompany/moviesapi/repository/MovieRepository.java
@@ -4,5 +4,4 @@ import com.mycompany.moviesapi.model.Movie;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MovieRepository extends MongoRepository<Movie, String> {
-
 }
