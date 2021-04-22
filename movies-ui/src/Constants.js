d8d737a60139e70c501aa4c40b406ed71b470e03
Ivan Franchin <ivan.franchin@takeaway.com>
2021-04-22 12:17:46 +0200

    project update - update spring-boot to 2.4.5; - update react to 17.0.2; - update springdoc-openapi to 1.5.7; - update mysql docker image to 5.7.34; - update mongo docker image to 4.4.5; - update avatars dicebear url.

--- movies-ui/src/Constants.js
+++ movies-ui/src/Constants.js
@@ -3,7 +3,7 @@ const prod = {
     KEYCLOAK_BASE_URL: "https://keycloak.herokuapp.com",
     API_BASE_URL: 'https://myapp.herokuapp.com',
     OMDB_BASE_URL: 'https://www.omdbapi.com',
-    AVATARS_DICEBEAR_URL: 'https://avatars.dicebear.com/v2'
+    AVATARS_DICEBEAR_URL: 'https://avatars.dicebear.com/api'
   }
 }
 
@@ -12,7 +12,7 @@ const dev = {
     KEYCLOAK_BASE_URL: "http://localhost:8080",
     API_BASE_URL: 'http://localhost:9080',
     OMDB_BASE_URL: 'https://www.omdbapi.com',
-    AVATARS_DICEBEAR_URL: 'https://avatars.dicebear.com/v2'
+    AVATARS_DICEBEAR_URL: 'https://avatars.dicebear.com/api'
   }
 }
 
