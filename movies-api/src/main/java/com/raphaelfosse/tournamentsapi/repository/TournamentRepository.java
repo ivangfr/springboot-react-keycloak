58ee7ddf86289021a3e3728ec908a72cdeabf9dc
Raphaelfosse <raphael.fosse91@gmail.com>
2023-08-15 18:03:27 +0200

    feat: files moved from movies-api to tournaments-api

--- movies-api/src/main/java/com/raphaelfosse/tournamentsapi/repository/TournamentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ivanfranchin.tournamentsapi.repository;
-
-import com.ivanfranchin.tournamentsapi.model.Tournament;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TournamentRepository extends MongoRepository<Tournament, String> {
-}
