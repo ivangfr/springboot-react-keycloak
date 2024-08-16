ce21ebbc290e66880834ccbe43d39ba7b513a53d
Ivan Franchin <ivan.franchin@takeaway.com>
2024-08-16 14:55:12 +0200

    Project upgrade - upgrade to keycloak 25.0.2; - upgrade to mongo docker image 7.0.12; - upgrade to postgre docker image 16.3; - upgrade npm packages.

--- init-environment.sh
+++ init-environment.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-MONGO_VERSION="7.0.6"
-POSTGRES_VERSION="16.1"
-KEYCLOAK_VERSION="25.0.1"
+MONGO_VERSION="7.0.12"
+POSTGRES_VERSION="16.3"
+KEYCLOAK_VERSION="25.0.2"
 
 source scripts/my-functions.sh
 
