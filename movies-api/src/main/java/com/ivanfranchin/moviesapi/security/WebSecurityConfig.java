dbceb3e2b047b12fb30423e2bd36cec9b0c5a9a5
Ivan Franchin <ivan.franchin@takeaway.com>
2024-11-28 23:42:04 +0100

    Remove unnecessary annotation in WebSecurityConfig

--- movies-api/src/main/java/com/ivanfranchin/moviesapi/security/WebSecurityConfig.java
+++ movies-api/src/main/java/com/ivanfranchin/moviesapi/security/WebSecurityConfig.java
@@ -6,13 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
 
     private final JwtAuthConverter jwtAuthConverter;
